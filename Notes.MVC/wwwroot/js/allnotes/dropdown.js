75eae0ce416294238f65b5f3e0bc0a21ddc365f7
KleerZ <escapeobzory@gmail.com>
2022-04-02 09:38:10 +0300

    Some little changes

---
+++ Notes.MVC/wwwroot/js/allnotes/dropdown.js
@@ -0,0 +1,56 @@
+let elem = 0;
+let style = 0;
+
+window.onload = function(){
+    myFunction();
+}
+
+function myFunction(){
+    elem = document.getElementById("dropdown")
+    style = elem.style.visibility;
+
+    let btn = document.getElementById('create-btn')
+    if (style == 'hidden'){
+        btn.style.background = '#483dff'
+
+        elem.style.visibility='visible';
+        elem.style.opacity='1';
+        anime({
+            targets: '.dropdown',
+            translateY: 7,
+            duration: 700
+        });
+
+        hide();
+    }
+    else{
+        anime({
+            targets: '.dropdown',
+            translateY: -15,
+            duration: 700
+        });
+        elem.style.opacity='0';
+        elem.style.visibility='hidden';
+        btn.style.background = '#6c63ff'
+    }
+
+    function hide(){
+        $(document).click('on', function(e){
+            var box = $('.drop-div');
+
+            if (!box.is(e.target) && box.has(e.target).length === 0){
+                let elem = document.querySelector('.dropdown');
+                let btn = document.getElementById('create-btn')
+                anime({
+                    targets: '.dropdown',
+                    translateY: -15,
+                    duration: 700
+                });
+
+                elem.style.opacity='0';
+                elem.style.visibility='hidden';
+                btn.style.background = '#6c63ff'
+            }
+        })
+    }
+}
